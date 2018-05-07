93fda7783540c0b09706df2864ee1461f94bbff6
Sesdika Sansani <sesdika.sansani@gmail.com>
2018-05-07 13:06:04 +0700

    Hapus2 file tidak terpakai

--- application/controllers/html.php
+++ application/controllers/html.php
@@ -16,8 +16,8 @@ class Html extends CI_Controller {
 		$this->load->library('session');
 		$this->load->library('tank_auth');
 		$this->load->database();
-		$this->load->library('dao/user_role_dao');
-		$this->load->library('dao/role_dao');
+		// $this->load->library('dao/user_role_dao');
+		// $this->load->library('dao/role_dao');
 		$this->logged_in();
 	}
 	
@@ -28,16 +28,7 @@ class Html extends CI_Controller {
 	}
 
 	public function index() {
-
 		$this->load->view('html/side-menu');
-	}
-
-	public function operasi() {
-		$this->load->view('html/form-operasi');
-	}
-
-	public function lapsithar() {
-		$this->load->view('html/lapsithar');
 	}
 
 	public function map_clean() {
@@ -69,132 +60,4 @@ class Html extends CI_Controller {
 		} else
 			$this->data['permission'] = '';
 	}
-
-	public function backend() {
-		$this->load->view('html/back-end');
-	}
-
-	public function dashboard() {
-		$this->load->view('html/dashboard');
-	}
-
-	public function justmaps() {
-		$this->load->view('html/justmaps');
-	}
-
-	public function pdfgenerator() {
-		$this->load->view('html/pdf-generator');
-	}
-
-	public function perintahmalam() {
-		$this->load->view('html/pdf-perintah-malam');
-	}
-
-	public function pdfperintahmalam() {
-		$this->load->helper('dompdf');
-
-		$html = $this->load->view('html/pdf-perintah-malam', '', true);
-		pdf_create($html, "Perintah Malam");
-	}
-
-	public function laporansiaga() {
-		$this->load->view('html/pdf-laporan-siaga');
-	}
-
-	public function pdflaporansiaga() {
-		$this->load->helper('dompdf');
-
-		$html = $this->load->view('html/pdf-laporan-siaga', '', true);
-		pdf_create($html, "Laporan Siaga");
-	}
-
-	public function lampiranlaporansiaga() {
-		$this->load->view('html/pdf-lampiran-laporan-siaga');
-	}
-
-	public function pdflampiranlaporansiaga() {
-		$this->load->helper('dompdf');
-
-		$html = $this->load->view('html/pdf-lampiran-laporan-siaga', '', true);
-		pdf_create($html, "Lampiran Laporan Siaga");
-	}
-
-	public function dislokasiKRI() {
-		$this->load->helper('dompdf');
-		$this->load->library('dao/ship_dao');
-		$ships = $this->ship_dao->table_fetch();
-		$this->data['ships'] = $ships;
-		$html = $this->load->view('html/dislokasi-KRI', $this->data, true);
-		pdf_create($html, "Dislokasi Kapal");
-	}
-
-	public function dislokasiPESUD() {
-		$this->load->helper('dompdf');
-		$this->load->library('dao/aeroplane_dao');
-		$aeroplanes = $this->aeroplane_dao->table_fetch();
-		$this->data['aeroplanes'] = $aeroplanes;
-		$html = $this->load->view('html/dislokasi-PESUD', $this->data, true);
-		pdf_create($html, "Dislokasi Pesawat");
-	}
-
-	public function dislokasiMARINIR() {
-		$this->load->helper('dompdf');
-		$this->load->library('dao/marines_dao');
-		$marines = $this->marines_dao->table_fetch();
-		$this->data['marines'] = $marines;
-		$html = $this->load->view('html/dislokasi-MARINIR', $this->data, true);
-		pdf_create($html, "Dislokasi Marinir");
-	}
-
-	public function pdflaporandislokasi() {
-		$this->load->helper('dompdf');
-
-		$html = $this->load->view('html/pdf-laporan-dislokasi', '', true);
-		pdf_create($html, "Laporan Dislokasi");
-	}
-
-	public function laporanhariankesiapankal() {
-		$this->load->view('html/pdf-laporan-harian-kesiapan-kal');
-	}
-
-	public function pdflaporanhariankesiapankal() {
-		$this->load->helper('dompdf');
-
-		$html = $this->load->view('html/pdf-laporan-harian-kesiapan-kal', '', true);
-		pdf_create($html, "Laporan Harian Kesiapan KAL");
-	}
-
-	public function laporanhariankesiapankri() {
-		$this->load->view('html/pdf-laporan-harian-kesiapan-kri');
-	}
-
-	public function pdflaporanhariankesiapankri() {
-		$this->load->helper('dompdf');
-
-		$html = $this->load->view('html/pdf-laporan-harian-kesiapan-kri', '', true);
-		pdf_create($html, "Laporan Harian Kesiapan KRI");
-	}
-
-	public function laporanhariankesiapanpesud() {
-		$this->load->view('html/pdf-laporan-harian-kesiapan-pesud');
-	}
-
-	public function pdflaporanhariankesiapanpesud() {
-		$this->load->helper('dompdf');
-
-		$html = $this->load->view('html/pdf-laporan-harian-kesiapan-pesud', '', true);
-		pdf_create($html, "Laporan Harian Kesiapan PESUD");
-	}
-
-	public function laporanhariankesiapanranpurmarinir() {
-		$this->load->view('html/pdf-laporan-harian-kesiapan-ranpur-marinir');
-	}
-
-	public function pdflaporanhariankesiapanranpurmarinir() {
-		$this->load->helper('dompdf');
-
-		$html = $this->load->view('html/pdf-laporan-harian-kesiapan-ranpur-marinir', '', true);
-		pdf_create($html, "Laporan Harian Kesiapan RANPUR MARINIR");
-	}
-
 }
